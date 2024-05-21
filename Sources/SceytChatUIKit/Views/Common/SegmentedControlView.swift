b1ccecacb2d7bbf29f15d7ea479fc25414b731e2
Arthur Avagyan <arthur.avagyan>
2024-05-21 18:46:04 +0400

    Make SegmentedControlView.SectionItem initializer public

--- Sources/SceytChatUIKit/Views/Common/SegmentedControlView.swift
+++ Sources/SceytChatUIKit/Views/Common/SegmentedControlView.swift
@@ -154,7 +154,7 @@ open class SegmentedControlView: View,
 }
 
 public extension SegmentedControlView {
-    struct SectionItem {
+    public struct SectionItem {
         public let content: UIView
         public let title: String
     }
