19abe2a758423fefddbe85e92062bd3a960abbae
norayrsct <norayr@sceyt.com>
2025-05-13 19:07:06 +0400

    Changed chat sdk version

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["SceytChatUIKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/sceyt/sceyt-chat-ios-sdk.git", branch: "refactor/vahagn/update-signaling")
+        .package(url: "https://github.com/sceyt/sceyt-chat-ios-sdk.git", from: "1.5.12"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
