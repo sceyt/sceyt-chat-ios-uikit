d017c3c5f30817d1e02e7787bac6c12dd138cc0b
Sargis Mkhitaryan <sargis.mkhitaryan@varmtech.com>
2026-04-21 15:03:05 +0400

    Update download file too.

--- Sources/SceytChatUIKit/ViewModels/Channel/GlobalSearchAllFilesViewModel.swift
+++ Sources/SceytChatUIKit/ViewModels/Channel/GlobalSearchAllFilesViewModel.swift
@@ -288,6 +288,15 @@ open class GlobalSearchAllFilesViewModel: NSObject {
     open func resumeDownload(_ layout: MessageLayoutModel.AttachmentLayout) {
         let attachment = layout.attachment
         guard attachment.type != "link" else { return }
+        if fileProvider.filePath(attachment: attachment) != nil {
+            DataProvider.database.write {
+                let dto = AttachmentDTO.fetch(id: attachment.id, context: $0)
+                dto?.status = ChatMessage.Attachment.TransferStatus.done.rawValue
+            } completion: { error in
+                logger.errorIfNotNil(error, "")
+            }
+            return
+        }
         getMessage(layout) { message in
             if let message {
                 fileProvider.resumeTransfer(message: message, attachment: attachment) {
