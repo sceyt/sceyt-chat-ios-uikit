18a6b084ee76c41da86515572418cae23b0f4d21
Sargis Mkhitaryan <sargis.mkhitaryan@varmtech.com>
2026-04-22 17:50:02 +0400

    Show from first selected page as each time start global search from the start.

--- Sources/SceytChatUIKit/Views/Channel/ChannelListViewController.swift
+++ Sources/SceytChatUIKit/Views/Channel/ChannelListViewController.swift
@@ -635,6 +635,12 @@ private extension ChannelListViewController {
 }
 
 extension ChannelListViewController: UISearchControllerDelegate {
+    public func willPresentSearchController(_ searchController: UISearchController) {
+        if let globalVC = searchResultsViewController as? GlobalSearchResultsViewController {
+            globalVC.selectPage(at: 0, animated: false)
+        }
+    }
+
     public func didDismissSearchController(_ searchController: UISearchController) {
         if let channel = pendingOpenChannel {
             channelListRouter.showChannelViewController(channel: channel)
