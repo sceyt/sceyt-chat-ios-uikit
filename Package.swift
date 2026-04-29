6aa11d7513def2677c78e8965ba2ffd6132ec9cb
Vahagn Manasyan <vahagnsct@sceyt.com>
2026-04-29 18:00:31 +0400

    chore: update ChatSDK

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
             targets: ["SceytChatUIKit"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/sceyt/sceyt-chat-ios-sdk.git", branch: "v1.5.18-beta.5"),
+        .package(url: "https://github.com/sceyt/sceyt-chat-ios-sdk.git", branch: "v1.5.18-beta.6"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
