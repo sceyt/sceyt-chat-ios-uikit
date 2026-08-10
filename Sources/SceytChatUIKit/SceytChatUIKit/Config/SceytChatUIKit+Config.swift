2c68525098e0b225b54520025fafc55b99dec022
Sargis Mkhitaryan <sargis.mkhitaryan@varmtech.com>
2026-08-10 12:48:43 +0400

    Fix defaultReactions heart emoji issue.

--- Sources/SceytChatUIKit/SceytChatUIKit/Config/SceytChatUIKit+Config.swift
+++ Sources/SceytChatUIKit/SceytChatUIKit/Config/SceytChatUIKit+Config.swift
@@ -97,7 +97,7 @@ extension SceytChatUIKit {
         public var mentionTriggerPrefix: String = "@"
         public var preventDuplicateAttachmentUpload: Bool = false
         public var messageBubbleTransformScale = CGAffineTransform(scaleX: 0.95, y: 0.95)
-        public var defaultReactions = ["👍", "😍", "❤️", "🤝", "😂", "😏"]
+        public var defaultReactions = ["👍", "😍", "❤", "🤝", "😂", "😏"]
         public var voiceRecorderConfig: VoiceRecorderConfig = .init()
     }
 }
